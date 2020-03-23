7b4a40ff42719ea89b39d86f58be90554a264fee
think-in-universe <mstr.raphael@gmail.com>
2020-03-23 23:07:10 +0800

    update config

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(notification-api.steem.buzz)
+domains=(notification.steem.buzz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="steem.guides@gmail.com" # Adding a valid address is strongly recommended
